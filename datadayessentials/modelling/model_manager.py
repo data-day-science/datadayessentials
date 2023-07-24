7f14cd04a84d0afe37b11bbc4784879f4af7c6d8
Chris Pickford <Chris.Pickford@carfinance247.co.uk>
2023-07-24 15:39:27 +0100

    added retries for model loads

--- datadayessentials/modelling/model_manager.py
+++ datadayessentials/modelling/model_manager.py
@@ -19,11 +19,8 @@ class ModelManager(IModelManager):
         resource_group (str): Resource group of the Azure ML studio
     """
 
-    def __init__(
-        self
-    ) -> None:
+    def __init__(self) -> None:
         self.workspace = get_workspace()
-
 
     def get_model_files_from_run(self, run_id: str, folder_to_save_model: str = None):
         """
@@ -43,7 +40,7 @@ class ModelManager(IModelManager):
             else folder_to_save_model
         )
         self.workspace.get_run(run_id).download_files(
-            output_directory=folder_to_save_model
+            output_directory=folder_to_save_model, timeout_seconds=60
         )
         return folder_to_save_model
 
@@ -73,12 +70,18 @@ class ModelManager(IModelManager):
         )
 
         model = Model(self.workspace, model_name, version=model_version)
+        max_retries = 5
+        for retries in range(1, max_retries + 1):
+            try:
+                model.download(target_dir=folder_to_save_model, exist_ok=True)
+                break
+            except Exception as e:
+                if retries == max_retries:
+                    raise e
+                print(f"failed to download model: attempt {retries}. Error was {e}")
 
-        model.download(
-            target_dir=folder_to_save_model, exist_ok=True
-        )
         return folder_to_save_model
-    
+
     def get_model_properties_from_run(self, run_id: str):
         """
         Gets the model properties from the run
@@ -90,7 +93,7 @@ class ModelManager(IModelManager):
             mlflow_manager.get_model_properties_from_run("2cf2751a-e2b8-44a7-bad0-a3f2d923cb2e")
         """
         return self.workspace.get_run(run_id).properties
-    
+
     def get_model_properties_from_registered_model(
         self, model_name: str, model_version: int = None
     ):
@@ -106,7 +109,7 @@ class ModelManager(IModelManager):
         """
         model = Model(self.workspace, model_name, version=model_version)
         return model.properties
-    
+
     def get_model_run_id_from_registered_model(
         self, model_name: str, model_version: int = None
     ):
@@ -123,7 +126,11 @@ class ModelManager(IModelManager):
         return Model(self.workspace, model_name, version=model_version).run_id
 
     def register_model_from_local_folder(
-        self, model_name: str, model_path: str, model_version: int = None, properties: dict = None
+        self,
+        model_name: str,
+        model_path: str,
+        model_version: int = None,
+        properties: dict = None,
     ) -> Model:
         """
         Registers the model in the Azure ML studio
@@ -143,11 +150,16 @@ class ModelManager(IModelManager):
             model_name=model_name,
             model_path=model_path,
             tags={"version": model_version},
-            properties=properties
+            properties=properties,
         )
 
     def register_model_from_run_id(
-        self, run_id: str, model_name: str, model_path: str = 'model', tags: dict = None, properties: dict = None
+        self,
+        run_id: str,
+        model_name: str,
+        model_path: str = "model",
+        tags: dict = None,
+        properties: dict = None,
     ) -> Model:
         """
         Registers the model from the run id in the Azure ML studio, without downloading the model files locally
@@ -164,5 +176,8 @@ class ModelManager(IModelManager):
 
         """
         return self.workspace.get_run(run_id).register_model(
-            model_name=model_name, model_path=model_path, tags=tags, properties=properties
+            model_name=model_name,
+            model_path=model_path,
+            tags=tags,
+            properties=properties,
         )

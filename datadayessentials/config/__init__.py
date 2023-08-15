7ba32898fa55a06ec268dfe43b7816f3466de417
Colin-Hanley <colin.hanley@carfinance247.co.uk>
2023-08-15 17:15:26 +0100

    committing progress

--- datadayessentials/config/__init__.py
+++ datadayessentials/config/__init__.py
@@ -1,9 +1,9 @@
 """
 This module provides classes for managing the core local config file, including for synchronizing the config file with a remote version stored in a machine learning workspace.
 """
-from ._config import CloudProviderConfig
+from ._config import Config
 
 
 __all__ = [
-    'CloudProviderConfig',
+    'Config',
 ]

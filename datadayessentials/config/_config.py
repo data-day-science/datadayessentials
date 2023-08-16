83ef07de4ba239a3341c00ed4ce37be04bcb1d51
Colin-Hanley <colin.hanley@carfinance247.co.uk>
2023-08-16 16:13:28 +0100

    refactor without tests

--- datadayessentials/config/_config.py
+++ datadayessentials/config/_config.py
@@ -2,30 +2,51 @@ import dataclasses
 import enum
 import platform
 from typing import Union
-
-from azure.core.exceptions import ServiceRequestError
-from azure.identity import DefaultAzureCredential
 from datascience_core.authentications import DataLakeAuthentication
 from azure.appconfiguration import AzureAppConfigurationClient
 import os
+from datadayessentials.config._execution_environment_manager import ExecutionEnvironmentManager
 
 
-def do_something():
-    """
-    # TODO: remove once replacement implemented
-    Placeholder function for demonstration purposes.
-    """
-    pass
+class AzureConfigManager:
 
+    def __init__(self, use_local_config: bool = False, base_url: str = None):
+        self.use_local_config = use_local_config
+        self.base_url = base_url
 
-class ExecutionEnvironment(enum.Enum):
-    DEV = "development"
-    PROD = "production"
-    LOCAL = "local"
+    def get_config_variable(self, key: str):
+        if self.use_local_config:
+            return self.get_config_variable_from_local(key)
+        else:
+            return self.get_config_variable_from_cloud(key)
+
+    def get_config_variable_from_local(self, key: str) -> Union[str, None]:
+        raise NotImplementedError("Local config not implemented yet")
+
+    def get_config_variable_from_cloud(self, key: str):
+        execution_env = ExecutionEnvironmentManager.get_execution_environment()
+
+        if execution_env == ExecutionEnvironment.PROD:
+            client = AzureAppConfigurationClient.from_connection_string(
+                connection_string=os.getenv("AZURE_APP_CONFIG_CONNECTION_STRING"))
+        elif execution_env == ExecutionEnvironment.DEV:
+            client = AzureAppConfigurationClient.from_connection_string(
+                connection_string=os.getenv("AZURE_APP_CONFIG_CONNECTION_STRING"))
+        elif execution_env == ExecutionEnvironment.LOCAL:
+            client = AzureAppConfigurationClient(
+                base_url=self.base_url,
+                credential=DataLakeAuthentication().get_credentials())
+        else:
+            raise ValueError(f"Environment {execution_env} not recognised")
+
+        variable_value = client.get_configuration_setting(key=key, label=execution_env.value)
+
+        return variable_value.next().value
 
 
 @dataclasses.dataclass
 class AzureAppConfigValues:
+    __dataclass_fields__ = None
     client_id: str = ""
     client_secret: str = ""
     data_lake: str = ""
@@ -45,17 +66,11 @@ class Config:
 
     """
 
-    def __init__(self, use_local_config: bool = False):
+    def __init__(self, use_local_config: bool = False, base_url: str = None):
         """
         Initializes the CloudProviderConfig instance.
         """
-        self.azure_client_app_client = None
-        self.base_url = None
-        self.environment = self.get_environment()
-        self.use_local_config = use_local_config
-
-    def set_base_url(self, base_url: str):
-        self.base_url = base_url
+        self.azure_config_manager = AzureConfigManager(use_local_config=use_local_config, base_url=base_url)
 
     def get_environment_variable(self, variable_name: str) -> str:
         """
@@ -71,74 +86,13 @@ class Config:
             ValueError:If the specified environment variable is not found in local or cloud configuration after re-getting
              variables.
         """
-        if self.use_local_config:
-            # search for yaml file in base directory
-            pass
 
         if os.getenv(variable_name):
             return os.getenv(variable_name)
         else:
-            # authenticate to azure
+            variable_value = self.azure_config_manager.get_config_variable(variable_name)
+            os.environ[variable_name] = variable_value
+            return variable_value
 
-            # set values to dataclass
-            # set environment variables
-            # return value.
-            azure_client = self.create_azure_app_configuration_client_connection(self.base_url)
-            configuration_settings = self.parse_and_save_client_items_to_azure_dataclass(azure_client)
-            self.set_environment_variables(configuration_settings)
-
-            if os.getenv(variable_name):
-                return os.getenv(variable_name)
-            else:
-                raise ValueError(
-                    f"Environment variable '{variable_name}' not found.\n"
-                    f"Make sure the variable is properly set in your cloud configuration."
-                )
-
-    @staticmethod
-    def create_azure_app_configuration_client_connection(base_url: str = None):
-
-        azure_app_config_client_connection = AzureAppConfigurationClient(
-            base_url=base_url,
-            credential=DataLakeAuthentication().get_credentials())
-
-        try:
-            azure_app_config_client_connection.list_configuration_settings().next()
-            return azure_app_config_client_connection
-
-        except Exception as e:
-            raise ServiceRequestError("Unable to create connection to AzureAppConfigurationClient with current "
-                                      "base_url attribute") from e
-
-    @staticmethod
-    def get_environment() -> ExecutionEnvironment:
-        """
-        Checks whether the environment is set to production.
-
-        Returns:
-            bool: True if the environment is production, False otherwise.
-        """
-
-        if platform.system() == 'Windows':
-            return ExecutionEnvironment.LOCAL
-        return ExecutionEnvironment.PROD if os.getenv("AZURE_ENVIRONMENT_NAME") == "prod" else ExecutionEnvironment.DEV
-
-    @staticmethod
-    def parse_and_save_client_items_to_azure_dataclass(client):
-        azure_values = AzureAppConfigValues()
-        local_environment = Config.get_environment()
-
-        for item in client.list_configuration_settings():
-            config_key = item.key
-            environment_flag = item.label
-            config_value = item.value
-
-            if environment_flag == local_environment:
-                setattr(azure_values, config_key, config_value)
-
-        return azure_values
-
-    @staticmethod
-    def set_environment_variables(azure_dataclass):
-        for key, value in vars(azure_dataclass).items():
-            os.environ[key.upper()] = value
+    def set_default_variables(self, list_of_variables: list = AzureAppConfigValues.__dataclass_fields__.keys()):
+        map(self.get_environment_variable, list_of_variables)

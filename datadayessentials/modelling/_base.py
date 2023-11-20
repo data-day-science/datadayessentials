e09790e462a3cf260e1985447f14bba1b775b6d0
Chris Pickford <Chris.Pickford@carfinance247.co.uk>
2023-11-20 13:23:46 +0000

    removing incorrect file

--- datadayessentials/modelling/_base.py
+++ datadayessentials/modelling/_base.py
@@ -166,40 +166,4 @@ class IModelEvaluator(ABC):
         """
         pass
 
-    class IInferenceModel(ABC):
-        """Interface for inference model"""
-
-        feature_names_: List[str] = None
-        feature_importances_: List[float] = None
-        model = None
-
-        @abstractmethod
-        def __init__(self, model):
-            if not model.model:
-                raise ValueError(
-                    "Model has not been fit.  Only a trained model should be passed to this class"
-                )
-
-        @abstractmethod
-        def predict(self, X: pd.DataFrame) -> np.array:
-            """Predicts the class for the given input data
-
-            Args:
-                X (pd.DataFrame): model input data.  Should be processed and in the correct format for the model to accept
-
-            Returns:
-                np.array: array of predictions for the given input data
-            """
-            pass
-
-        @abstractmethod
-        def predict_proba(self, X: pd.DataFrame) -> np.array:
-            """Predicts the class probabilities for the given input data
-
-            Args:
-                X (pd.DataFrame): model input data.  Should be processed and in the correct format for the model to accept
-
-            Returns:
-                np.array: array of class probabilities for the given input data
-            """
-            pass
+    
